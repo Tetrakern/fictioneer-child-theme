04e20e6b0a832c591f81d867fabda0e580ad45df
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2023-07-25 18:42:51 +0200

    Add some more examples

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 // CONSTANTS
 // =============================================================================
 
-define( 'CHILD_VERSION', '1.0.0' );
+define( 'CHILD_VERSION', '1.0.1' );
 define( 'CHILD_NAME', 'Fictioneer Child Theme' );
 
 // =============================================================================
@@ -38,5 +38,37 @@ function fictioneer_child_enqueue_styles_and_scripts() {
   wp_enqueue_script( 'fictioneer-child-scripts' );
 }
 add_action( 'wp_enqueue_scripts', 'fictioneer_child_enqueue_styles_and_scripts', 99 );
+
+/**
+ * Remove or customize parent filters and actions on the frontend
+ */
+
+function fictioneer_child_customize_parent() {
+  /*
+
+  Example: Prevent custom story/chapter CSS from being applied
+
+  remove_action( 'wp_head', 'fictioneer_add_fiction_css', 10 );
+
+  */
+}
+add_action( 'init', 'fictioneer_child_customize_parent' );
+
+/**
+ * Remove or customize parent filters and actions in the admin panel
+ */
+
+function fictioneer_child_customize_admin() {
+  /*
+
+  Example: Remove SEO meta box for non-administrators
+
+  if ( ! current_user_can( 'administrator' ) ) {
+    remove_action( 'add_meta_boxes', 'fictioneer_add_seo_metabox', 10 );
+  }
+
+  */
+}
+add_action( 'admin_init', 'fictioneer_child_customize_admin' );
 
 ?>
